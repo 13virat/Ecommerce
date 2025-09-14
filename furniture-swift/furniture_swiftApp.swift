18a4c6ea8270337a83eb7d686da235a797c2e311
Virat Gupta <viratgupta1314@gmail.com>
2025-09-14 23:11:01 +0530

    Initial Commit

---
+++ furniture-swift/furniture_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  furniture_swiftApp.swift
+//  furniture-swift
+//
+//  Created by Mac pro on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct furniture_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
