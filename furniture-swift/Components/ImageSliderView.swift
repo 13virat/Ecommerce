ae39c961d82147fdf92ab55291507c13c73c639b
Virat Gupta <viratgupta1314@gmail.com>
2025-09-25 00:39:15 +0530

    BUG FIX

--- furniture-swift/Components/ImageSliderView.swift
+++ furniture-swift/Components/ImageSliderView.swift
@@ -1,42 +1,41 @@
-//
-//  ImageSliderView.swift
-//  furniture-swift
-//
-//  Created by Mac pro on 20/09/25.
-//
-
 import SwiftUI
 
 struct ImageSliderView: View {
     @State private var currentIndex = 0
     var slides: [String] = ["fn1","fn2","fn3","fn4","fn5"]
+    
     var body: some View {
-        ZStack(alignment: .bottomLeading){
-            ZStack(alignment: .trailing){
+        ZStack(alignment: .bottomLeading) {
+            ZStack(alignment: .trailing) {
                 Image(slides[currentIndex])
                     .resizable()
-                    .frame(width: .infinity, height: 180)
-                    .scaledToFit()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: 180)
                     .cornerRadius(15)
             }
-            HStack{
-                ForEach(0..<slides.count){
-                    index in Circle()
+            
+            HStack {
+                ForEach(slides.indices, id: \.self) { index in
+                    Circle()
                         .fill(self.currentIndex == index ? Color("kPrimary") : Color("kSecondary"))
                         .frame(width: 10, height: 10)
                 }
-            }.padding()
-        }.padding()
-            .onAppear{Timer.scheduledTimer(withTimeInterval: 5, repeats: true){
-            timer in if self.currentIndex + 1 == self.slides.count{
-                self.currentIndex = 0
-            }else{
-                self.currentIndex += 1
             }
+            .padding()
         }
+        .padding()
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+                if self.currentIndex + 1 == self.slides.count {
+                    self.currentIndex = 0
+                } else {
+                    self.currentIndex += 1
+                }
+            }
         }
     }
 }
+
 struct ImageSliderView_Previews: PreviewProvider {
     static var previews: some View {
         ImageSliderView()
